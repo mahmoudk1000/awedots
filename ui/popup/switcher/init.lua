7517a8dc481648c41131a169618be924055a7df8
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-09-12 11:56:50 +0300

    popup.switcher: revamp

--- ui/popup/switcher/init.lua
+++ ui/popup/switcher/init.lua
@@ -5,159 +5,147 @@ local wibox = require("wibox")
 local dpi = beautiful.xresources.apply_dpi
 
 local function createElements()
-    local elems = wibox.widget({
-        layout = wibox.layout.fixed.horizontal,
-        spacing = 20,
-        id = "switcher",
-    })
+	local elements = wibox.widget({
+		spacing = dpi(20),
+		layout = wibox.layout.fixed.horizontal,
+	})
 
-    local current = 1
+	local current = 1
 
-    local function updateElements(action)
-        action = action or ""
-        elems:reset()
+	local function updateElements(action)
+		action = action or ""
+		elements:reset()
 
-        local clients = client.get()
-        local sortedClients = {}
+		local clients = client.get()
+		local sortedClients = {}
 
-        if client.focus then
-            table.insert(sortedClients, client.focus)
-        end
+		if client.focus then
+			table.insert(sortedClients, client.focus)
+		end
 
-        for _, c in ipairs(clients) do
-            if c ~= sortedClients[1] then
-                table.insert(sortedClients, c)
-            end
-        end
+		for _, c in ipairs(clients) do
+			if c ~= sortedClients[1] then
+				table.insert(sortedClients, c)
+			end
+		end
 
-        for _, c in ipairs(sortedClients) do
-            local widget = wibox.widget({
-                {
-                    {
-                        {
-                            id = "icon",
-                            forced_height = 70,
-                            halign = "center",
-                            forced_width = 70,
-                            image = c.icon,
-                            widget = wibox.widget.imagebox,
-                        },
-                        {
-                            {
-                                id = "name",
-                                halign = "center",
-                                text = c.name or "Unnamed",
-                                widget = wibox.widget.textbox,
-                            },
-                            widget = wibox.container.constraint,
-                            width = 100,
-                            height = 18,
-                        },
-                        spacing = dpi(5),
-                        layout = wibox.layout.fixed.vertical,
-                    },
-                    margins = dpi(20),
+		for _, c in ipairs(sortedClients) do
+			local widget = wibox.widget({
+				{
+					{
+						nil,
+						{
+							image = c.icon,
+							halign = "center",
+							widget = wibox.widget.imagebox,
+						},
+						{
+							text = c.name or "Unnamed",
+							halign = "center",
+							forced_height = dpi(10),
+							widget = wibox.widget.textbox,
+						},
+						layout = wibox.layout.align.vertical,
+					},
+					margins = dpi(10),
 					widget = wibox.container.margin,
-                },
-				bg = beautiful.bg,
-				forced_width = dpi(130),
-                forced_height = dpi(135),
-                shape = helpers:rrect(),
+				},
+				bg = beautiful.xbackground,
+				forced_width = dpi(120),
+				forced_height = dpi(120),
+				shape = helpers:rrect(),
 				widget = wibox.container.background,
-            })
-            elems:add(widget)
-        end
+			})
+			elements:add(widget)
+		end
 
-        if action == "next" then
-            current = (current >= #sortedClients) and 1 or current + 1
-        end
+		if action == "next" then
+			current = (current >= #sortedClients) and 1 or current + 1
+		end
 
-        for i, element in ipairs(elems.children) do
-            element.bg = (i == current) and beautiful.xcolor8 or beautiful.xbackground
-        end
+		for i, element in ipairs(elements.children) do
+			element.bg = (i == current) and beautiful.xcolor8 or beautiful.xbackground
+		end
 
-        if action == "raise" then
-            local c = sortedClients[current]
-            if c then
-                if not c:isvisible() and c.first_tag then
-                    c.first_tag:view_only()
-                end
-                c:emit_signal("request::activate")
-                c:raise()
-            end
-            current = 0
-        end
+		if action == "raise" then
+			local c = sortedClients[current]
+			if c then
+				if not c:isvisible() and c.first_tag then
+					c.first_tag:view_only()
+				end
+				c:emit_signal("request::activate")
+				c:raise()
+			end
+			current = 0
+		end
 
-        return elems
-    end
+		return elements
+	end
 
-    elems = updateElements()
+	elements = updateElements()
 
-    return elems, updateElements
+	return elements, updateElements
 end
 
-local function winswitch(s)
-    local winlist, updateElements = createElements()
+awful.screen.connect_for_each_screen(function(s)
+	local winlist, updateElements = createElements()
 
-    local container = awful.popup({
+	local container = awful.popup({
 		screen = s,
-        ontop = true,
-        visible = false,
-        stretch = false,
-		bg = beautiful.bg,
-        shape = helpers:rrect(),
-        placement = awful.placement.centered,
+		ontop = true,
+		visible = false,
+		stretch = false,
+		bg = beautiful.xbackground,
+		shape = helpers:rrect(),
+		placement = awful.placement.centered,
 		widget = wibox.container.background,
-    })
+	})
 
-    container:setup({
-        {
+	container:setup({
+		{
 			winlist,
-            margins = dpi(10),
+			margins = dpi(10),
 			widget = wibox.container.margin,
-        },
-        layout = wibox.layout.fixed.vertical,
-    })
+		},
+		layout = wibox.layout.fixed.vertical,
+	})
 
-    local function showSwitcher()
-        container.visible = true
-        updateElements()
-    end
+	local function showSwitcher()
+		container.visible = true
+		updateElements("next")
+	end
 
-    local function hideSwitcher()
-        updateElements("raise")
-        container.visible = false
-    end
+	local function hideSwitcher()
+		updateElements("raise")
+		container.visible = false
+	end
 
-    local keygrabber
-    keygrabber = awful.keygrabber({
-        start_callback = showSwitcher,
-        stop_callback = function()
-            hideSwitcher()
-        end,
-        keybindings = {
-            {
-                { "Mod4" },
-                "Tab",
-                function()
-                    updateElements("next")
-                end,
-                { description = "Switch to next window", group = "client" },
-            },
-        },
-        stop_key = "Mod4",
-        stop_event = "release",
-        export_keybindings = true,
-        stop_on_event = true,
-    })
+	local keygrabber = awful.keygrabber({
+		start_callback = showSwitcher,
+		stop_callback = function()
+			hideSwitcher()
+		end,
+		keybindings = {
+			{
+				{ "Mod4" },
+				"Tab",
+				function()
+					updateElements("next")
+				end,
+				{ description = "Switch to Next Window", group = "Client" },
+			},
+		},
+		stop_key = "Mod4",
+		stop_event = "release",
+		export_keybindings = true,
+		stop_on_event = true,
+	})
 
-    awful.keyboard.append_global_keybindings({
-        awful.key({ "Mod4" }, "Tab", function()
-            if not keygrabber.is_running then
-                keygrabber:start()
-            end
-        end, { description = "Open window switcher", group = "client" })
-    })
-end
-
-awful.screen.connect_for_each_screen(winswitch)
+	awful.keyboard.append_global_keybindings({
+		awful.key({ "Mod4" }, "Tab", function()
+			if not keygrabber.is_running then
+				keygrabber:start()
+			end
+		end, { description = "Open window switcher", group = "client" }),
+	})
+end)
