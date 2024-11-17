2458119bdc30fd528d765a476a1c24f0ac8ef01d
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-11-17 16:40:33 +0200

    signal(volume): fix logic

--- signal/volume.lua
+++ signal/volume.lua
@@ -7,8 +7,8 @@ local M = {}
 function M:emit_volume_state()
 	awful.spawn.easy_async_with_shell("pamixer --get-volume;pamixer --get-mute", function(stdout)
 		local percent = tonumber(stdout:match("(.-)\n"))
-		local is_muted = stdout:match("\n(true)\n") ~= nil
-		local icon = nil
+		local is_muted = stdout:match("\n(true)") or false
+		local icon
 
 		if percent == nil or percent == "" then
 			percent = tonumber(0)
@@ -16,12 +16,14 @@ function M:emit_volume_state()
 
 		if is_muted then
 			icon = icons.volume.mute
-		elseif percent >= 70 then
-			icon = icons.volume.high
-		elseif percent >= 35 then
-			icon = icons.volume.medium
 		else
-			icon = icons.volume.low
+			if percent >= 70 then
+				icon = icons.volume.high
+			elseif percent >= 35 then
+				icon = icons.volume.medium
+			else
+				icon = icons.volume.low
+			end
 		end
 
 		awesome.emit_signal("volume::value", percent, icon, is_muted)
